6659207fb97fb2c36d5f4afe2f0c553fc0f40598
PDA-T <1581109028@qq.com>
2022-11-12 22:32:30 +0800

    新增配置中心

--- hello-spring-cloud-alibaba-nacos-provider/src/main/java/com/pda/hello/spring/cloud/alibaba/nacos/provider/controller/NacosProviderController.java
+++ hello-spring-cloud-alibaba-nacos-provider/src/main/java/com/pda/hello/spring/cloud/alibaba/nacos/provider/controller/NacosProviderController.java
@@ -1,6 +1,8 @@
 package com.pda.hello.spring.cloud.alibaba.nacos.provider.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +20,10 @@ public class NacosProviderController {
 	@Value(value = "${server.port}")
 	private String port;
 
+	// 配置中心上下文
+	@Autowired
+	private ConfigurableApplicationContext applicationContext;
+
 	/**
 	 * @author PDA
 	 * @Date 2022/11/4 21:33
@@ -29,5 +35,16 @@ public class NacosProviderController {
 	@GetMapping(value = "/echo/{message}")
 	public String echo(@PathVariable(value = "message") String message){
 		return "hello" + message + " port:" + port;
+	}
+
+	/**
+	 * @Date 2022/11/12 22:29
+	 * @Description 获取配置中心新增的动态配置
+	 * @return java.lang.String
+	 * @since version-1.0
+	 */
+	@GetMapping(value = "/hi")
+	public String hi(){
+		return applicationContext.getEnvironment().getProperty("user.name");
 	}
 }
